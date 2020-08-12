467caf767301fa0cf3212c309e293d03db3ffda5
Atm Mushfiq Mahmud <62173091+mmrafid@users.noreply.github.com>
2020-08-12 08:48:09 +0600

    Update signup.php

--- signup.php
+++ signup.php
@@ -12,14 +12,14 @@
 	<form action="registration.php" method="post">
 	
 	<label for=""> User name </label>
-	<input type="text" name="user" class="form-control" required/>
+	<input type="text" name="user" class="form-control" placeholder="Insert a unique name"required/>
 	
 	<br />
 	<br />
 	
 	
 	<label for=""> Password </label>
-	<input type="password" name="password" class="form-control" required/>
+	<input type="password" name="password" class="form-control" placeholder="Max length 8" required/>
 	
 	<br />
 	<br />
