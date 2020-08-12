c69d30100ec910ff831a5eeff53ff2f835a44faa
Atm Mushfiq Mahmud <62173091+mmrafid@users.noreply.github.com>
2020-08-12 08:50:20 +0600

    Update login.php

--- login.php
+++ login.php
@@ -12,14 +12,14 @@
 	<form action="validation.php" method="post">
 	
 	<label for=""> User name </label>
-	<input type="text" name="user" class="form-control" required/>
+	<input type="text" name="user" class="form-control" placeholder="Your username" required/>
 	
 	<br />
 	<br />
 	
 	
 	<label for=""> Password </label>
-	<input type="password" name="password" class="form-control" required/>
+	<input type="password" name="password" class="form-control" placeholder="Max 8 digit password" required/>
 	
 	<br />
 	<br />
